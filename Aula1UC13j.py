9bd1a3014cd7eb9561a181dc49ccd15976e09b80
Edineusa Gomes <edineusamariagomes@gmail.com>
2025-02-06 19:27:34 -0300

    arquivos da aula

---
+++ Aula1UC13j.py
@@ -0,0 +1,15 @@
+#num1 = 18+12+8j
+#num2 = 50 + 5j
+#print(num1)
+
+
+#nome = input('Digite seu nome: ')
+#print(nome)
+
+#idade = input('Digite seu nome: ')
+#print(idade)
+
+nome = input('Digite seu nome: ')
+idade = input('Digite seu idade: ')
+print(nome, '_' ,type(nome))
+print(idade, '_' ,type(idade))
